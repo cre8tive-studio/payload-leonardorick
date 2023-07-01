ef40f71a36d46eb52817443f86c970048708b6a5
Leonardo Rick <leonardo.rick@unbabel.com>
2023-07-01 11:50:01 +0100

    update server to use port from env

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import payload from 'payload';
+import { mediaManagement } from 'payload-cloudinary-plugin';
 
 require('dotenv').config();
 const app = express();
@@ -9,6 +10,20 @@ app.get('/', (_, res) => {
   res.redirect('/admin');
 });
 
+app.use(
+  mediaManagement(
+    {
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    },
+    {
+      folder: process.env.CLOUDINARY_FOLDER,
+      use_filename: true,
+      resource_type: 'image',
+    }
+  )
+);
 const start = async () => {
   // Initialize Payload
   await payload.init({
@@ -16,13 +31,13 @@ const start = async () => {
     mongoURL: process.env.MONGODB_URI,
     express: app,
     onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
     },
-  })
+  });
 
   // Add your own express routes here
 
-  app.listen(4000);
-}
+  app.listen(process.env.PORT || 4000);
+};
 
 start();
