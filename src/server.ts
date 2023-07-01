a41289c05a7d052019ea267cee4a0c7dad8b4af4
Leonardo Rick <leonardo.rick@unbabel.com>
2023-07-01 11:55:54 +0100

    comment cloudinary

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import payload from 'payload';
-import { mediaManagement } from 'payload-cloudinary-plugin';
+// import { mediaManagement } from 'payload-cloudinary-plugin';
 
 require('dotenv').config();
 const app = express();
@@ -10,20 +10,20 @@ app.get('/', (_, res) => {
   res.redirect('/admin');
 });
 
-app.use(
-  mediaManagement(
-    {
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    },
-    {
-      folder: process.env.CLOUDINARY_FOLDER,
-      use_filename: true,
-      resource_type: 'image',
-    }
-  )
-);
+// app.use(
+//   mediaManagement(
+//     {
+//       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//       api_key: process.env.CLOUDINARY_API_KEY,
+//       api_secret: process.env.CLOUDINARY_API_SECRET,
+//     },
+//     {
+//       folder: process.env.CLOUDINARY_FOLDER,
+//       use_filename: true,
+//       resource_type: 'image',
+//     }
+//   )
+// );
 const start = async () => {
   // Initialize Payload
   await payload.init({
