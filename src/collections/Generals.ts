71adfaf97259654bd43ca06be603fd987450b1a6
Leonardo Rick <leonardo.rick@unbabel.com>
2024-08-23 21:56:58 +0100

    add options to fields in generals so htmlTag and animationType only select proper values

--- src/collections/Generals.ts
+++ src/collections/Generals.ts
@@ -19,6 +19,42 @@ const Generals: CollectionConfig = {
       type: 'array',
       fields: [
         {
+          name: 'htmlTag',
+          type: 'select',
+          options: [
+            {
+              value: 'p',
+              label: '<p>',
+            },
+            {
+              value: 'h1',
+              label: '<h1>',
+            },
+            {
+              value: 'h2',
+              label: '<h2>',
+            },
+            {
+              value: 'h3',
+              label: '<h3>',
+            },
+          ],
+        },
+        {
+          name: 'animationType',
+          type: 'select',
+          options: [
+            {
+              value: 'none',
+              label: 'None',
+            },
+            {
+              value: 'fadeOpacity',
+              label: 'Fade Opacity',
+            },
+          ],
+        },
+        {
           name: 'text',
           type: 'richText',
           localized: true,
